d436530b1608d836dddba1fa6ef179c799942f49
Vladimir N <evaeva6202@gmail.com>
2024-11-06 07:20:34 +0300

    module_4_1.py  fake_math.py  true_math.py
    module_4_1.py   fake_math.py  true_math.py

---
+++ fake_math.py
@@ -0,0 +1,7 @@
+def divide(first, second):
+    if second != 0:
+        d = first/second
+        return d
+    else:
+        return 'Ошибка'
+
---
+++ module_4_1.py
@@ -0,0 +1,7 @@
+from fake_math import divide as fuk_div
+from true_math import divide as tru_div
+print('result 1 = ', fuk_div(69, 3))
+print('result 2 = ', fuk_div(3, 0))
+print('result 3 = ', tru_div(49, 7))
+print('result 4 = ', tru_div(15, 0))
+
